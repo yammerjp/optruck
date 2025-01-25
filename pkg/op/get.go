f4fe9d8dfcad967dc2467bd1eb861b147394e386
Keisuke Nakayama <me@yammer.jp>
2025-01-25 13:11:05 +0900

    refactor(op): move CheckItemExists function from check.go to get.go for better organization and maintainability

--- pkg/op/get.go
+++ pkg/op/get.go
@@ -49,3 +49,12 @@ func (c *Client) GetItem(itemName string) (*GetItemResponse, error) {
 
 	return &resp, nil
 }
+
+func (c *Client) CheckItemExists(itemName string) (bool, error) {
+	item, err := c.GetItem(itemName)
+	if err != nil {
+		return false, err
+	}
+
+	return item != nil, nil
+}
