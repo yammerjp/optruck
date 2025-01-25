00edd7a6141651cb1554e09ebb5ffd8eda8ebc62
Keisuke Nakayama <me@yammer.jp>
2025-01-25 22:55:59 +0900

    add comments to run op account list / op vault list

---
+++ pkg/op/account.go
@@ -0,0 +1,13 @@
+package op
+
+/*
+$ op account list --format json
+[
+  {
+    "url": "my.1password.com",
+    "email": "mail@example.com",
+    "user_uuid": "0123456789ABCDEFGH",
+    "account_uuid": "ABCDEFGH0123456789"
+  }
+]
+*/
